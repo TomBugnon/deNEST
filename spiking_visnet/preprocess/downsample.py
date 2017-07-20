caec1df858a89b7d7511767c123f6aa7418b1b87
Tom Bugnon <tombugnon@hotmail.com>
2017-07-20 17:52:55 -0500

    Code downsampling for x*y*t np arrays using scipy.misc.imresize

--- spiking_visnet/preprocess/downsample.py
+++ spiking_visnet/preprocess/downsample.py
@@ -3,9 +3,20 @@
 # downsample.py
 
 
+import numpy as np
+import scipy.misc
+
+
 # TODO
 def downsample(input_movie, preprocessing_params, network):
-    return input_movie
+
+    xdim, ydim = network.input_res()
+    tdim = np.size(input_movie, axis=2)
+    output_movie = np.zeros((xdim, ydim, tdim))
+    for t in range(tdim):
+        output_movie[:, :, t] = scipy.misc.imresize(input_movie[:, :, t],
+                                                    (xdim, ydim))
+    return output_movie
 
 
 def get_string(_, network):
