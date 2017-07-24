e477f131b9cd52ce48bcfbc4df088eec145ab866
Tom Bugnon <tombugnon@hotmail.com>
2017-07-24 11:29:06 -0500

    Define "save_as_sparse" and "load_as_numpy" methods (still TODO)

--- spiking_visnet/save.py
+++ spiking_visnet/save.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # save.py
+
+
+import nest
+
+from ..user_config import SAVE_DIR
+
+
+# TODO:
+def save_as_sparse(path, array):
+    """ Saves 3D (raw movie) or 4D (preprocessed movie) np array as scipy sparse
+    array in path """
+    pass
+
+
+# TODO:
+def load_as_numpy(path):
+    """ Loads file as np array. Path can point towards either a numpy file or
+    a 'scipy' file saved using save_as_sparse() """
+    pass
