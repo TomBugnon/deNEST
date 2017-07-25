894789cd1acc5ff7925c702566cab4748b314550
Tom Bugnon <tombugnon@hotmail.com>
2017-07-25 12:26:15 -0500

    move sparsification to separate module in utils

--- spiking_visnet/save.py
+++ spiking_visnet/save.py
@@ -3,22 +3,15 @@
 # save.py
 
 
+"""Save and load movies, networks, activity and simulation parameters."""
+
+
 import nest
 
 from ..user_config import SAVE_DIR
 
 
-# TODO:
-def save_as_sparse(path, array):
-    """Save nparrays as scipy sparse array in <path>."""
-    pass
 
 
-# TODO:
-def load_as_numpy(path):
-    """Load file at <path> as np array.
 
-    Path can point towards either a numpy file or a 'scipy' file saved using
-    save_as_sparse().
     """
-    pass
---
+++ spiking_visnet/utils/sparsify.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# sparsify.py
+
+
+"""Compress, save and load 3D and 4D np arrays"""
+
+
+import numpy as np
+
+
+# TODO:
+def save_as_sparse(path, array):
+    """Save 3D or 4D nparrays as scipy sparse array in <path>."""
+    pass
+
+
+# TODO:
+def load_as_numpy(path):
+    """Load file at <path> as np array.
+
+    Path can point towards either a numpy file or a 'scipy' file saved using
+    save_as_sparse().
+    """
+    pass
