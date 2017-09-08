13f0b9a2dc16cb6a3ea3737b0cd55d0f152b3de4
Will Mayner <wmayner@gmail.com>
2017-09-07 20:12:22 -0500

    Re-implement sparse save/load

--- spiking_visnet/utils/sparsify.py
+++ spiking_visnet/utils/sparsify.py
@@ -4,30 +4,51 @@
 
 """Save and load binary arrays sparsely."""
 
-import numpy as np
+import os
+import pickle
+
+import scipy.sparse
+
+
+# Use LIL as the default sparse format
+sparse_format = scipy.sparse.lil_matrix
+
+
+def ensure_ext(path, ext='.pkl'):
+    """Add a file extension if there isn't one."""
+    path, _ext = os.path.splitext(path)
+    _ext = _ext or ext
+    return path + _ext
 
 
 # TODO: rename to save_sparse
 def save_as_sparse(path, array):
-    """Save a binary array in a sparse format.
-
-    .. note::
-        This assumes the array contains only binary values.
-    """
-    indices = np.where(array == 1)
-    np.savez(path, indices)
+    """Save an array in a sparse format."""
+    # Normalize file extension
+    path = ensure_ext(path, ext='.pkl')
+    # Store shape
+    shape = array.shape
+    # Ensure 2D
+    array = array.reshape(shape[0], -1)
+    # Save
+    data = {'shape': shape,
+            'data': sparse_format(array)}
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
+    return True
 
 
 # TODO: Rename to load_sparse
 # TODO: implement another function that accepts either dense or sparse
 def load_as_numpy(path):
-    """Load a sparse array.
-
-    Path can point towards either a numpy file or a scipy' file saved using
-    save_as_sparse().
-    """
-    indices = np.load(path)
-    shape = (index.size for index in indices)
-    dense = np.zeros(shape, dtype=int)
-    dense[indices] = 1
-    return dense
+    """Load an array saved with ``save_as_sparse``."""
+    # Normalize file extension
+    path = ensure_ext(path, ext='.pkl')
+    # Load
+    with open(path, 'rb') as f:
+        loaded = pickle.load(f)
+    shape, data = loaded['shape'], loaded['data']
+    # Convert to dense
+    data = data.toarray()
+    # Reshape
+    return data.reshape(shape)
