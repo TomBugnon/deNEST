bb1505e90f57e11ca9fc92dc9caed434a2619094
Tom Bugnon <tombugnon@hotmail.com>
2019-10-15 10:41:06 -0500

    Refactor nets.save

--- nets/save.py
+++ nets/save.py
@@ -43,106 +43,9 @@ OUTPUT_SUBDIRS = {'params': (),
 # is set to true.
 CLEAR_SUBDIRS = [subdir for subdir in OUTPUT_SUBDIRS.values()]
 
-def output_subdir(output_dir, data_keyword, session_name=None, create_dir=True):
-    """Create and return the output subdirectory where a data type is saved.
-
-    Args:
-        output_dir (str): path to the main output directory for a simulation.
-        data_keyword (str): String designating the type of data for which we
-            return an output subdirectory. Should be a key of the OUTPUT_SUBDIRS
-            dictionary.
-        session_name (str or None): If a session is provided, data is organized
-            by subdirectories with that session's name.
-    """
-    if session_name is None:
-        subdir = join(output_dir, *OUTPUT_SUBDIRS[data_keyword])
-    else:
-        subdir = join(output_dir, *OUTPUT_SUBDIRS[data_keyword], session_name)
-    if create_dir:
-        os.makedirs(subdir, exist_ok=True)
-    return subdir
-
-
-def output_filename(data_keyword, *args, **kwargs):
-    """Return the filename under which a type of data is saved.
-
-    Args:
-        data_keyword (str): String designating the type of data for which we
-            return a filename.
-        *args: Optional arguments passed to the function generating a filename
-            for a given data type.
-    """
-    return FILENAME_FUNCS[data_keyword](*args, **kwargs)
-
-
-def output_path(output_dir, data_keyword, *args, session_name=None, **kwargs):
-    return join(output_subdir(output_dir,
-                              data_keyword,
-                              session_name=session_name),
-                output_filename(data_keyword, *args, **kwargs))
-
-
-def make_output_dir(output_dir, clear_output_dir=True,
-                    delete_subdirs_list=None):
-    """Create and possibly clear output directory.
-
-    Create the directory if it doesn't exist.
-    If `clear_output_dir` is True, we clear the directory. We iterate over all
-    the subdirectories specified in CLEAR_SUBDIRS, and for each of those we:
-        - delete all the files
-        - delete all the directories whose name is in the `delete_subdirs` list.
-
-    Args:
-        output_dir (str):
-        clear_output_dir (bool): Whether we clear the CLEAR_SUBDIRS
-        delete_subdirs_list (list of str or None): List of subdirectories of
-            CLEAR_SUBDIRS that we delete.
-    """
-    if delete_subdirs_list is None:
-        delete_subdirs_list = []
-    os.makedirs(output_dir, exist_ok=True)
-    if clear_output_dir:
-        for clear_dir in [join(output_dir, *subdir)
-                          for subdir in CLEAR_SUBDIRS
-                          if isdir(join(output_dir, *subdir))]:
-            print(f'-> Clearing {clear_dir}')
-            # Delete files in the CLEAR_SUBDIRS
-            delete_files(clear_dir)
-            # Delete the contents of all the delete_subdirs we encounter
-            delete_subdirs(clear_dir, delete_subdirs_list)
-
-
-def delete_files(clear_dir):
-    """Delete all files in a directory."""
-    for f in os.listdir(clear_dir):
-        path = join(clear_dir, f)
-        if os.path.isfile(path):
-            os.remove(path)
-
-def delete_subdirs(clear_dir, delete_subdirs_list):
-    """Delete some subdirectories in a directory."""
-    for f in os.listdir(clear_dir):
-        path = join(clear_dir, f)
-        if os.path.isdir(path) and f in delete_subdirs_list:
-            shutil.rmtree(path)
-
-def save_array(path, array):
-    """Save array either as dense or sparse depending on data type."""
-    try:
-        save_sparse(path, array)
-    except TypeError:
-        np.save(path, array)
-
-# TODO: fix pickle.dump failure for large files
-def save_dict(path, dictionary):
-    """Save a big dic with pickle."""
-    try:
-        with open(path, 'wb') as f:
-            pickle.dump(dictionary, f)
-    except OSError as e:
-        msg = (f"Could not save data at {f}! \n"
-               f"... Ignoring the following error: {str(e)}`")
-        warnings.warn(msg)
+################################
+#### Utility functions for data loading
+################################
 
 def load_dict(path):
     """Load a big dic with pickle."""
@@ -156,12 +59,6 @@ def load_as_numpy(path):
     if ext == '.npy':
         return np.load(path)
     return load_sparse(path)
-
-
-def save_as_yaml(path, tree):
-    """Save <tree> as yaml file at <path>."""
-    with open(path, 'w') as f:
-        yaml.dump(tree, f, default_flow_style=False)
 
 
 def load_yaml(*args):
@@ -497,12 +394,6 @@ def load_measure(output_dir, measure_name, session=None, start_trim=None,
         times = range(min(times), max(times) - int(end_trim))
     return measure[times]
 
-def save_measure(output_dir, measure, measure_name):
-    """Save measure as np-array (eg LFP)."""
-    path = join(output_subdir(output_dir, 'measures'), measure_name)
-    np.save(path, measure)
-    return path
-
 # TODO: fix pickle.dump failure for large files
 def save_sparse(path, array):
     """Save an array in a sparse format."""
@@ -544,6 +435,139 @@ def load_sparse(path):
     data = data.toarray()
     # Reshape
     return data.reshape(shape)
+################################
+#### Utility functions for data saving
+################################
+
+def save_as_yaml(path, tree):
+    """Save <tree> as yaml file at <path>."""
+    with open(path, 'w') as f:
+        yaml.dump(tree, f, default_flow_style=False)
+
+def save_array(path, array):
+    """Save array either as dense or sparse depending on data type."""
+    try:
+        save_sparse(path, array)
+    except TypeError:
+        np.save(path, array)
+
+# TODO: fix pickle.dump failure for large files
+def save_dict(path, dictionary):
+    """Save a big dic with pickle."""
+    try:
+        with open(path, 'wb') as f:
+            pickle.dump(dictionary, f)
+    except OSError as e:
+        msg = (f"Could not save data at {f}! \n"
+               f"... Ignoring the following error: {str(e)}`")
+        warnings.warn(msg)
+
+
+def save_measure(output_dir, measure, measure_name):
+    """Save measure as np-array (eg LFP)."""
+    path = join(output_subdir(output_dir, 'measures'), measure_name)
+    np.save(path, measure)
+    return path
+
+
+def save_plot(fig, output_dir, filename, overwrite=False):
+    """Save matplotlib figure in 'plot' subdirectory."""
+    filename = filename.replace('.', ',')
+    path = join(output_subdir(output_dir, 'plots'), filename)
+    if os.path.exists(path) and not overwrite:
+        print(f'Not overwriting file at {path}')
+        return
+    fig.savefig(path)
+
+
+################################
+#### Paths, filenames and output directory organisation
+################################
+
+
+def output_subdir(output_dir, data_keyword, session_name=None, create_dir=True):
+    """Create and return the output subdirectory where a data type is saved.
+
+    Args:
+        output_dir (str): path to the main output directory for a simulation.
+        data_keyword (str): String designating the type of data for which we
+            return an output subdirectory. Should be a key of the OUTPUT_SUBDIRS
+            dictionary.
+        session_name (str or None): If a session is provided, data is organized
+            by subdirectories with that session's name.
+    """
+    if session_name is None:
+        subdir = join(output_dir, *OUTPUT_SUBDIRS[data_keyword])
+    else:
+        subdir = join(output_dir, *OUTPUT_SUBDIRS[data_keyword], session_name)
+    if create_dir:
+        os.makedirs(subdir, exist_ok=True)
+    return subdir
+
+
+def output_filename(data_keyword, *args, **kwargs):
+    """Return the filename under which a type of data is saved.
+
+    Args:
+        data_keyword (str): String designating the type of data for which we
+            return a filename.
+        *args: Optional arguments passed to the function generating a filename
+            for a given data type.
+    """
+    return FILENAME_FUNCS[data_keyword](*args, **kwargs)
+
+
+def output_path(output_dir, data_keyword, *args, session_name=None, **kwargs):
+    return join(output_subdir(output_dir,
+                              data_keyword,
+                              session_name=session_name),
+                output_filename(data_keyword, *args, **kwargs))
+
+
+def make_output_dir(output_dir, clear_output_dir=True,
+                    delete_subdirs_list=None):
+    """Create and possibly clear output directory.
+
+    Create the directory if it doesn't exist.
+    If `clear_output_dir` is True, we clear the directory. We iterate over all
+    the subdirectories specified in CLEAR_SUBDIRS, and for each of those we:
+        - delete all the files
+        - delete all the directories whose name is in the `delete_subdirs` list.
+
+    Args:
+        output_dir (str):
+        clear_output_dir (bool): Whether we clear the CLEAR_SUBDIRS
+        delete_subdirs_list (list of str or None): List of subdirectories of
+            CLEAR_SUBDIRS that we delete.
+    """
+    if delete_subdirs_list is None:
+        delete_subdirs_list = []
+    os.makedirs(output_dir, exist_ok=True)
+    if clear_output_dir:
+        for clear_dir in [join(output_dir, *subdir)
+                          for subdir in CLEAR_SUBDIRS
+                          if isdir(join(output_dir, *subdir))]:
+            print(f'-> Clearing {clear_dir}')
+            # Delete files in the CLEAR_SUBDIRS
+            delete_files(clear_dir)
+            # Delete the contents of all the delete_subdirs we encounter
+            delete_subdirs(clear_dir, delete_subdirs_list)
+
+
+def delete_files(clear_dir):
+    """Delete all files in a directory."""
+    for f in os.listdir(clear_dir):
+        path = join(clear_dir, f)
+        if os.path.isfile(path):
+            os.remove(path)
+
+
+def delete_subdirs(clear_dir, delete_subdirs_list):
+    """Delete some subdirectories in a directory."""
+    for f in os.listdir(clear_dir):
+        path = join(clear_dir, f)
+        if os.path.isdir(path) and f in delete_subdirs_list:
+            shutil.rmtree(path)
 
 
 def recorder_formatted_filename(layer, pop, unit_index=None, variable='spikes',
@@ -564,15 +588,6 @@ def recorder_metadata_filename(label):
     """Return filename for a recorder from its label."""
     return label
 
-
-def save_plot(fig, output_dir, filename, overwrite=False):
-    """Save matplotlib figure in 'plot' subdirectory."""
-    filename = filename.replace('.', ',')
-    path = join(output_subdir(output_dir, 'plots'), filename)
-    if os.path.exists(path) and not overwrite:
-        print(f'Not overwriting file at {path}')
-        return
-    fig.savefig(path)
 
 def movie_filename():
     return 'session_movie'
