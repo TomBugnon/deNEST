8ff98f567c1cf0e536c7b1a166e48a6a6fa6092f
TomBugnon <tombugnon@hotmail.com>
2020-02-26 18:12:30 +0100

    Remove unused code

--- nets/utils/misc.py
+++ nets/utils/misc.py
@@ -26,55 +26,7 @@ def drop_git_hash(output_dir):
         f.write(git_hash)
 
 
-def delete_contents(filename):
-    with open(filename, "wb"):
-        pass
-
-
 def git_head_hash():
     process = subprocess.Popen(['git', 'rev-parse', 'HEAD'], shell=False,
                                stdout=subprocess.PIPE)
     return process.communicate()[0].strip()
-
-
-def generate_output_subdir(main_output_dir, label=None,
-                           base_output_name='output_'):
-    """Return the path to a subdirectory of `main_output_dir` to use as output.
-
-    The returned path has the following structure::
-        `main_output_dir`/`base_output_name_`+`N`+ '_' + `date` + '_' + `label`
-    where:
-        - `N` is incremented at each run depending on the existing
-        subdirectories of `main_output_dir`
-        - `date` is of the form yyyy/mm/dd/hh:mm
-    """
-
-    # pylint:disable=invalid-name
-
-    import datetime
-    import os
-    import os.path
-
-    # Get N
-    # Get the name of all the subdirectories in main_output_dir that start with
-    # 'base_output_name'
-    all_subdirs = [filename[len(base_output_name):]
-                   for filename in os.listdir(main_output_dir)
-                   if os.path.isdir(os.path.join(main_output_dir,
-                                                 filename))
-                   and filename.startswith(base_output_name)]
-    print(all_subdirs)
-    all_subdirs_N = [int(subdir.split('_')[0]) for subdir in all_subdirs
-                     if subdir.split('_')[0].isdigit()]
-
-    N = 1 if not all_subdirs_N else max(all_subdirs_N) + 1
-
-    # Get time string
-    timestr = '{date:%Y-%m-%d}'.format(date=datetime.datetime.now())
-
-    # label string
-    labelstr = '' if label is None else '_' + label
-
-    output_subdir = base_output_name + str(N) + '_' + timestr + labelstr
-
-    return os.path.join(main_output_dir, output_subdir)
