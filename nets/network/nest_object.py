4fe4467845a1074b7e7ac7c6ad071483e82b799d
TomBugnon <tombugnon@hotmail.com>
2020-03-25 17:14:26 +0100

    Add nest_params Arg to NestObject

--- nets/network/nest_object.py
+++ nets/network/nest_object.py
@@ -5,6 +5,7 @@
 
 import functools
 from pprint import pformat
+import copy as cp
 
 
 @functools.total_ordering
@@ -13,17 +14,19 @@ class NestObject:
 
     Args:
         name (str): The name of the object.
-        params (Params): The object parameters.
+        params (Params): The object parameters. Interpreted by NETS.
+        nest_params (Params): The object parameters passed to NEST.
 
     Objects are ordered and hashed by name.
     """
 
     # pylint: disable=too-few-public-methods
 
-    def __init__(self, name, params):
+    def __init__(self, name, params, nest_params):
         self.name = name
         # Flatten the parameters to a dictionary (and make a copy)
-        self.params = dict(params)
+        self.params = cp.deepcopy(params)
+        self.nest_params = cp.deepcopy(nest_params)
         # Whether the object has been created in NEST
         self._created = False
 
@@ -35,13 +38,15 @@ class NestObject:
         with p.group(p.indentation, opener, closer):
             p.breakable()
             p.pretty(self.params)
+            p.pretty(self.nest_params)
 
     # pylint: enable=unused-argument,invalid-name
 
     def __repr__(self):
-        return '{classname}({name}, {params})'.format(
+        return '{classname}({name}, {params}, {nest_params})'.format(
             classname=type(self).__name__, name=self.name,
-            params=pformat(self.params))
+            params=pformat(self.params), nest_params=pformat(self.nest_params)
+        )
 
     def __lt__(self, other):
         return self.name < other.name
