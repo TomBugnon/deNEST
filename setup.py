2a7f6b497bcd306f157eec30e4131088584b827a
Will Mayner <wmayner@gmail.com>
2020-04-27 18:53:58 -0500

    setup.py: Update supported Python versions

--- setup.py
+++ setup.py
@@ -37,10 +37,9 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering',
     ]
 )
