f1d24d1726efa84290eaddff52cb40ecf7ce3aab
Will Mayner <wmayner@gmail.com>
2020-06-27 12:12:11 -0500

    docs: set `master_doc`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,16 +12,17 @@
 #
 import sys
 from pathlib import Path
-sys.path.insert(0, Path('...').resolve())
+
+sys.path.insert(0, Path("...").resolve())
 
 from denest import __author__, __version__
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'deNEST'
-copyright = '2020, Tom Bugnon and William G. P. Mayner'
-author = 'Tom Bugnon and William G. P. Mayner'
+project = "deNEST"
+copyright = "2020, Tom Bugnon and William G. P. Mayner"
+author = "Tom Bugnon and William G. P. Mayner"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,22 +31,24 @@ author = 'Tom Bugnon and William G. P. Mayner'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'nbsphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
 ]
 
+master_doc = "index"
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,26 +56,25 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
+
+html_theme = "sphinx_rtd_theme"
 # TODO
 # html_logo = "logo.png"
 # html_favicon = "logo.png"
 
 # Add any paths that contain custom themes here, relative to this
 # directory.
-html_theme_path = ['_themes'] + [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = ["_themes"] + [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'navigation_depth': 3
-}
+html_theme_options = {"navigation_depth": 3}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
